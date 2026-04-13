69ca2aa03a7faffdc427490637afe09ea7f63513
Jun Sun <jsun@junsun.net>
2026-04-13 03:44:31 +0000

    riscv64: log unhandled SBI exits instead of panicking
    The SBI exit handler called unimplemented!() which panics crosvm.
    KVM handles common SBI extensions (timer, IPI) in-kernel, but
    unknown extensions exit to userspace.  Log them instead of crashing.

--- src/crosvm/sys/linux/vcpu.rs
+++ src/crosvm/sys/linux/vcpu.rs
@@ -463,11 +463,11 @@ fn vcpu_loop(
                     thread::sleep(Duration::from_nanos(delay_ns));
                 }
                 Ok(VcpuExit::Sbi {
-                    extension_id: _,
-                    function_id: _,
-                    args: _,
+                    extension_id,
+                    function_id,
+                    args,
                 }) => {
-                    unimplemented!("Sbi exits not yet supported");
+                    error!("SBI exit: ext_id={:#x} func_id={:#x} args={:?}", extension_id, function_id, args);
                 }
                 Ok(VcpuExit::RiscvCsr {
                     csr_num,
